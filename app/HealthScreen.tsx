342045da352c828b570c1581fde2005fccc9457c
doyeon <duseh729@naver.com>
2025-09-22 20:09:56 +0900

    feat: 건강체크 화면 구현

--- app/HealthScreen.tsx
+++ app/HealthScreen.tsx
@@ -1,5 +1,13 @@
 import CommonHeader from "@/components/Common/CommonHeader";
 import { SafeAreaContainer } from "@/components/Common/SafeAreaContainer";
+import DailyReport from "@/components/HealthScreen/DailyReport";
+
+const data = [
+    {title: '체온 상태', imageUrl: require('assets/images/health-icon1.png'), contents: '열이 나거나 오한이 있나요?'},
+    {title: '정신 상태', imageUrl: require('assets/images/health-icon2.png'), contents: '어지럽거나 머리가 아픈가요?'},
+    {title: '신체 활동', imageUrl: require('assets/images/health-icon3.png'), contents: '몸에 힘이 있고 활동하기 괜찮은가요?'},
+    {title: '기분 상태', imageUrl: require('assets/images/health-icon4.png'), contents: '기분이 좋고 컨디션이 괜찮은가요?'},
+]
 
 export default function HealthScreen() {
   return (
@@ -9,6 +17,7 @@ export default function HealthScreen() {
         title="건강확인"
         contents="현재 시간"
       />
+      <DailyReport title="오늘 컨디션을 확인해주세요" contents={data} />
     </SafeAreaContainer>
   );
 }
---
+++ components/HealthScreen/DailyReport.tsx
@@ -0,0 +1,163 @@
+import { Colors } from "@/constants/Colors";
+import React, { useState } from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  StyleProp,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from "react-native";
+
+type menuItem = {
+  imageUrl: ImageSourcePropType;
+  title: string;
+  contents: string;
+};
+
+type reportProps = {
+  title: string;
+  contents: menuItem[];
+};
+
+export default function DailyReport({ title, contents }: reportProps) {
+  const [selections, setSelections] = useState<{
+    [key: number]: string | null;
+  }>({});
+
+  const handleSelect = (index: number, selection: string) => {
+    setSelections((prev) => {
+      const newSelections = { ...prev };
+      if (newSelections[index] === selection) {
+        newSelections[index] = null;
+      } else {
+        newSelections[index] = selection;
+      }
+      return newSelections;
+    });
+  };
+
+  return (
+    <View style={{ paddingHorizontal: 40, marginTop: 20, gap: 20 }}>
+      <Text
+        style={{ color: Colors.font.default, fontSize: 28, fontWeight: "700" }}
+      >
+        {title}
+      </Text>
+      {contents.map((value, index) => {
+        const selectedOption = selections[index];
+        const isGoodActive = selectedOption === "좋음";
+        const isBadActive = selectedOption === "좋지않음";
+
+        return (
+          <View
+            key={index}
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
+              <Image source={value.imageUrl}></Image>
+              <View style={{ flex: 1, paddingHorizontal: 10 }}>
+                <Text
+                  style={{
+                    color: Colors.font.default,
+                    fontWeight: "700",
+                    fontSize: 24,
+                  }}
+                >
+                  {value.title}
+                </Text>
+                <Text
+                  style={{
+                    color: Colors.font.gray,
+                    fontSize: 24,
+                    flexWrap: "wrap",
+                  }}
+                >
+                  {value.contents}
+                </Text>
+              </View>
+            </View>
+
+            <View
+              style={{ flexDirection: "row", gap: 20, marginTop: 10, flex1: 1 }}
+            >
+              <ReportButton
+                onPress={() => handleSelect(index, "좋음")}
+                imageUrl={
+                  isGoodActive
+                    ? require("assets/images/check-white.png")
+                    : require("assets/images/check-green.png")
+                }
+                text="좋음"
+                style={{
+                  backgroundColor: isGoodActive ? "#10B981" : "#F3F4F6",
+                  borderColor: "#10B981",
+                  borderWidth: 1,
+                }}
+                textStyle={{ color: isGoodActive ? "white" : "#10B981" }}
+              />
+              <ReportButton
+                onPress={() => handleSelect(index, "좋지않음")}
+                imageUrl={
+                  isBadActive
+                    ? require("assets/images/close.png")
+                    : require("assets/images/close-red.png")
+                }
+                text="좋지않음"
+                style={{
+                  backgroundColor: isBadActive ? "#EF4444" : "#F3F4F6",
+                  borderColor: "#EF4444",
+                  borderWidth: 1,
+                }}
+                textStyle={{ color: isBadActive ? "white" : "#EF4444" }}
+              />
+            </View>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+type buttonProps = {
+  imageUrl: ImageSourcePropType;
+  text: string;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+  onPress?: () => void;
+};
+
+const ReportButton = ({
+  imageUrl,
+  text,
+  style,
+  textStyle,
+  onPress,
+}: buttonProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        {
+          borderRadius: 12,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+          minWidth: 140,
+        },
+        style,
+      ]}
+    >
+      <Image source={imageUrl} />
+      <Text style={[{ fontSize: 20 }, textStyle]}>{text}</Text>
+    </Pressable>
+  );
+};
